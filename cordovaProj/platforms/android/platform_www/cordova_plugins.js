4fcd6c01d777371a1073c0d111a7b193e48bfcf8
wamisnet <wamiwami@live,jp>
2018-01-19 00:27:18 +0900

    1.2.6

--- cordovaProj/platforms/android/platform_www/cordova_plugins.js
+++ cordovaProj/platforms/android/platform_www/cordova_plugins.js
@@ -1,14 +1,6 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
 module.exports = [
   {
-    "id": "cordova-plugin-splashscreen.SplashScreen",
-    "file": "plugins/cordova-plugin-splashscreen/www/splashscreen.js",
-    "pluginId": "cordova-plugin-splashscreen",
-    "clobbers": [
-      "navigator.splashscreen"
-    ]
-  },
-  {
     "id": "cordova-plugin-customurlscheme.LaunchMyApp",
     "file": "plugins/cordova-plugin-customurlscheme/www/android/LaunchMyApp.js",
     "pluginId": "cordova-plugin-customurlscheme",
@@ -26,31 +18,39 @@ module.exports = [
     ]
   },
   {
-    "id": "cordova-plugin-statusbar.statusbar",
-    "file": "plugins/cordova-plugin-statusbar/www/statusbar.js",
-    "pluginId": "cordova-plugin-statusbar",
-    "clobbers": [
-      "window.StatusBar"
-    ]
-  },
-  {
     "id": "cordova-plugin-qrscanner.QRScanner",
     "file": "plugins/cordova-plugin-qrscanner/www/www.min.js",
     "pluginId": "cordova-plugin-qrscanner",
     "clobbers": [
       "QRScanner"
     ]
+  },
+  {
+    "id": "cordova-plugin-splashscreen.SplashScreen",
+    "file": "plugins/cordova-plugin-splashscreen/www/splashscreen.js",
+    "pluginId": "cordova-plugin-splashscreen",
+    "clobbers": [
+      "navigator.splashscreen"
+    ]
+  },
+  {
+    "id": "cordova-plugin-statusbar.statusbar",
+    "file": "plugins/cordova-plugin-statusbar/www/statusbar.js",
+    "pluginId": "cordova-plugin-statusbar",
+    "clobbers": [
+      "window.StatusBar"
+    ]
   }
 ];
 module.exports.metadata = 
 // TOP OF METADATA
 {
-  "cordova-plugin-whitelist": "1.3.3",
-  "cordova-plugin-splashscreen": "4.1.0",
   "cordova-plugin-customurlscheme": "4.3.0",
   "cordova-plugin-inappbrowser": "2.0.1",
+  "cordova-plugin-qrscanner": "2.5.0",
+  "cordova-plugin-splashscreen": "4.1.0",
   "cordova-plugin-statusbar": "2.4.1",
-  "cordova-plugin-qrscanner": "2.5.0"
+  "cordova-plugin-whitelist": "1.3.3"
 };
 // BOTTOM OF METADATA
 });
