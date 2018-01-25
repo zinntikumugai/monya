258e834eb8a46fa62d08e91ba8cdd9acc343b845
wamisnet <wamiwami@live,jp>
2018-01-25 19:51:14 +0900

    1.2.9

--- cordovaProj/platforms/android/assets/www/cordova_plugins.js
+++ cordovaProj/platforms/android/assets/www/cordova_plugins.js
@@ -18,9 +18,25 @@ module.exports = [
     ]
   },
   {
+    "id": "cordova-plugin-keychain-touch-id.TouchID",
+    "file": "plugins/cordova-plugin-keychain-touch-id/www/touchid.js",
+    "pluginId": "cordova-plugin-keychain-touch-id",
+    "clobbers": [
+      "window.plugins.touchid"
+    ]
+  },
+  {
     "id": "cordova-plugin-qrscanner.QRScanner",
     "file": "plugins/cordova-plugin-qrscanner/www/www.min.js",
     "pluginId": "cordova-plugin-qrscanner",
+    "clobbers": [
+      "QRScanner"
+    ]
+  },
+  {
+    "id": "cordova-plugin-qrscanner-monya.QRScanner",
+    "file": "plugins/cordova-plugin-qrscanner-monya/www/www.min.js",
+    "pluginId": "cordova-plugin-qrscanner-monya",
     "clobbers": [
       "QRScanner"
     ]
@@ -40,6 +56,14 @@ module.exports = [
     "clobbers": [
       "window.StatusBar"
     ]
+  },
+  {
+    "id": "cordova-clipboard.Clipboard",
+    "file": "plugins/cordova-clipboard/www/clipboard.js",
+    "pluginId": "cordova-clipboard",
+    "clobbers": [
+      "cordova.plugins.clipboard"
+    ]
   }
 ];
 module.exports.metadata = 
@@ -47,10 +71,13 @@ module.exports.metadata =
 {
   "cordova-plugin-customurlscheme": "4.3.0",
   "cordova-plugin-inappbrowser": "2.0.1",
+  "cordova-plugin-keychain-touch-id": "3.2.1",
   "cordova-plugin-qrscanner": "2.5.0",
+  "cordova-plugin-qrscanner-monya": "2.5.4",
   "cordova-plugin-splashscreen": "4.1.0",
   "cordova-plugin-statusbar": "2.4.1",
-  "cordova-plugin-whitelist": "1.3.3"
+  "cordova-plugin-whitelist": "1.3.3",
+  "cordova-clipboard": "1.1.1"
 };
 // BOTTOM OF METADATA
 });
